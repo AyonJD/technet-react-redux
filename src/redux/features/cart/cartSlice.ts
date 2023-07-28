0a18e67f682542b2c5a0edd458f34b2bad3d8abc
AyonJD <ayonjodder177@gmail.com>
2023-07-28 20:24:02 +0600

    Update total price in cart for add remove one and delete product from cart

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -1,13 +1,16 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { IProduct } from '@/types/globalTypes';
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
 interface ICart {
   products: IProduct[];
+  total: number;
 }
 
 const initialState: ICart = {
   products: [],
+  total: 0,
 };
 
 const cartSlice = createSlice({
@@ -19,25 +22,26 @@ const cartSlice = createSlice({
         (product) => product._id === action.payload._id
       );
       if (isExist) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         isExist.quantity = isExist.quantity! + 1;
       } else {
         state.products.push({ ...action.payload, quantity: 1 });
       }
+      state.total += action.payload.price;
     },
     removeOne: (state, action: PayloadAction<IProduct>) => {
       const isExist = state.products.find(
         (product) => product._id === action.payload._id
       );
       if (isExist && isExist.quantity! > 1) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         isExist.quantity = isExist.quantity! - 1;
+        state.total -= action.payload.price;
       }
     },
     removeCart: (state, action: PayloadAction<IProduct>) => {
       state.products = state.products.filter(
         (product) => product._id !== action.payload._id
       );
+      state.total -= action.payload.price * action.payload.quantity!;
     },
   },
 });
